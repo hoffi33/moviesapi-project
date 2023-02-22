5d03aa37b206f4e7e0c86554e8f39f423beb8683
s24551 <hoffmannjakub@icloud.com>
2023-02-22 14:19:35 +0100

    Added logic for adding review for selected movie

--- moviesAPI/src/main/java/pl/pjatk/moviesAPI/controllers/ReviewController.java
+++ moviesAPI/src/main/java/pl/pjatk/moviesAPI/controllers/ReviewController.java
@@ -1,7 +1,26 @@
 package pl.pjatk.moviesAPI.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.pjatk.moviesAPI.model.Review;
+import pl.pjatk.moviesAPI.services.ReviewService;
+
+import java.util.Map;
 
 @RestController
+@RequestMapping("/api/v1/reviews")
 public class ReviewController {
+    @Autowired
+    private ReviewService reviewService;
+
+    @PostMapping
+    public ResponseEntity<Review> createReview(@RequestBody Map<String, String> payload){
+        return new ResponseEntity<Review>(reviewService.createReview(payload.get("reviewBody"), payload.get("imdbId")), HttpStatus.OK);
+    }
 }
--- moviesAPI/src/main/java/pl/pjatk/moviesAPI/services/ReviewService.java
+++ moviesAPI/src/main/java/pl/pjatk/moviesAPI/services/ReviewService.java
@@ -1,12 +1,30 @@
 package pl.pjatk.moviesAPI.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
+import pl.pjatk.moviesAPI.model.Movie;
 import pl.pjatk.moviesAPI.model.Review;
+import pl.pjatk.moviesAPI.repositories.ReviewRepository;
 
 @Service
 public class ReviewService {
 
-    public Review createReview(String reviewBody, String imdbId){
-        Review review = new Review(reviewBody);
+    @Autowired
+    private ReviewRepository reviewRepository;
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    public Review createReview(String reviewBody, String imdbId) {
+        Review review = reviewRepository.insert(new Review(reviewBody));
+
+        mongoTemplate.update(Movie.class)
+                .matching(Criteria.where("imdbId").is(imdbId))
+                .apply(new Update().push("reviewIds").value(review))
+                .first();
+
+        return review;
     }
 }
