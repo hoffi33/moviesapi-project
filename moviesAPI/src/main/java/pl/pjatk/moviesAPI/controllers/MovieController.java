a2c7bf206c7d7f1149561c1941b747b28006521e
s24551 <hoffmannjakub@icloud.com>
2023-02-22 13:51:09 +0100

    Added logic for getting single movie by ObjectId

--- moviesAPI/src/main/java/pl/pjatk/moviesAPI/controllers/MovieController.java
+++ moviesAPI/src/main/java/pl/pjatk/moviesAPI/controllers/MovieController.java
@@ -1,17 +1,17 @@
 package pl.pjatk.moviesAPI.controllers;
 
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.pjatk.moviesAPI.model.Movie;
 import pl.pjatk.moviesAPI.services.MovieService;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/movies")
@@ -23,4 +23,10 @@ public class MovieController {
     public ResponseEntity<List<Movie>> getAllMovies() {
         return new ResponseEntity<List<Movie>>(movieService.allMovies(), HttpStatus.OK);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Movie>> getMovieById(@PathVariable ObjectId id){
+        return new ResponseEntity<Optional<Movie>>(movieService.movieById(id),HttpStatus.OK);
+    }
+
 }
--- moviesAPI/src/main/java/pl/pjatk/moviesAPI/services/MovieService.java
+++ moviesAPI/src/main/java/pl/pjatk/moviesAPI/services/MovieService.java
@@ -1,11 +1,13 @@
 package pl.pjatk.moviesAPI.services;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import pl.pjatk.moviesAPI.model.Movie;
 import pl.pjatk.moviesAPI.repositories.MovieRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
@@ -17,5 +19,8 @@ public class MovieService {
         return movieRepository.findAll();
     }
 
+    public Optional<Movie> movieById(ObjectId id){
+        return movieRepository.findById(id);
+    }
 
 }
