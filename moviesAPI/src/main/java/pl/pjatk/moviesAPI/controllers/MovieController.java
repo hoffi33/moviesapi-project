14c8a95846367901a956db201152429754223315
s24551 <hoffmannjakub@icloud.com>
2023-02-22 13:54:20 +0100

    Changed searching via ObjectId into String imdbId

--- moviesAPI/src/main/java/pl/pjatk/moviesAPI/controllers/MovieController.java
+++ moviesAPI/src/main/java/pl/pjatk/moviesAPI/controllers/MovieController.java
@@ -24,9 +24,9 @@ public class MovieController {
         return new ResponseEntity<List<Movie>>(movieService.allMovies(), HttpStatus.OK);
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Optional<Movie>> getMovieById(@PathVariable ObjectId id){
-        return new ResponseEntity<Optional<Movie>>(movieService.movieById(id),HttpStatus.OK);
+    @GetMapping("/{imdbId}")
+    public ResponseEntity<Optional<Movie>> getMovieById(@PathVariable String imdbId){
+        return new ResponseEntity<Optional<Movie>>(movieService.movieById(imdbId),HttpStatus.OK);
     }
 
 }
--- moviesAPI/src/main/java/pl/pjatk/moviesAPI/services/MovieService.java
+++ moviesAPI/src/main/java/pl/pjatk/moviesAPI/services/MovieService.java
@@ -19,8 +19,8 @@ public class MovieService {
         return movieRepository.findAll();
     }
 
-    public Optional<Movie> movieById(ObjectId id){
-        return movieRepository.findById(id);
+    public Optional<Movie> movieById(String imdbId){
+        return movieRepository.findMovieByImdbId(imdbId);
     }
 
 }
